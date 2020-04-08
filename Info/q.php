3468db71c729a2a7d3bd8bfdf7c941cfff37367e
Ghena <ghena85@gmail.com>
2020-04-08 19:12:58 +0300

    no message

--- Info/q.php
+++ Info/q.php
@@ -1,3 +1,5 @@
 1. Wishlist tabela vad ca nu este
 doar in (users_meta) user_wishlist
 2. De uitat in comentarii
+3. users table
+Registration Source = manual form or instagram
--- Info/todo.php
+++ Info/todo.php
@@ -1,1 +1,3 @@
 1. Check all fileds for all tables
+2. All users
+referred_by varchar [ref: > USER.referral_code] // code who referrer user

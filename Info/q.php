333f54238c0e348d0f6f917fdbc8033eec9fbe29
Ghena <ghena85@gmail.com>
2020-04-09 00:13:52 +0300

    no message

--- Info/q.php
+++ Info/q.php
@@ -4,3 +4,4 @@ doar in (users_meta) user_wishlist
 3. users table
 Registration Source = manual form or instagram
 4. Customer Meta User wishtlist
+5. Agent Payment and Artist Payment proccesed_by,reffered_by

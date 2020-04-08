8d25c729b478ed0e52d513090914d942966911e7
Ghena <ghena85@gmail.com>
2020-04-08 19:41:25 +0300

    no message

--- Info/q.php
+++ Info/q.php
@@ -3,3 +3,4 @@ doar in (users_meta) user_wishlist
 2. De uitat in comentarii
 3. users table
 Registration Source = manual form or instagram
+4. Customer Meta User wishtlist
--- Info/todo.php
+++ Info/todo.php
@@ -1,3 +1,10 @@
 1. Check all fileds for all tables
 2. All users
 referred_by varchar [ref: > USER.referral_code] // code who referrer user
+2. Admin Users Page
+http://crazy.lara/admin/admin-users
+4. Remove Name from edit users or list
+5. Customer meta nu se ved Bio in tabel
+http://crazy.lara/admin/user-meta
+
+

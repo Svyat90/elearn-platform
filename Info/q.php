41cb3112cec0eb9ef5d144af6c0ca144597d8fbc
Ghena <ghena85@gmail.com>
2020-04-28 12:02:35 +0300

    no message

--- Info/q.php
+++ Info/q.php
@@ -1,7 +1,12 @@
 1. Add tagline field which will store user tagline tex  ?
-A added an input with tagline name
+A added an input with tagline name,I hope it's ok.
 2. Permissions - If any Tables permission left kindly add it?
 Yes we have there all tables added.
 3.  If possible add date base report generation because admin need to generate from and to dates report in csv pdf etc to send it to celebrity
 You can export report(csv,excel,pdf) on each tables with select options.
 Do you an date range for all pages(Artist Payment) in rubric Payment Management ?
+4.
+Orders Completed 0% 0%
+Orders Processing 0% 0%
+Other Statuses 0%
+Could you explain how we ca control this in percents ?
